bc92113742833d3a54eb7d2a55e6820e71563e5d
halilozler <ozler166@hotmail.com>
2023-03-01 18:19:35 +0300

    task 7

---
+++ Task7/Net Core Patika/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Net_Core_Patika.DbOperations;
+
+namespace Net_Core_Patika.Application.AuthorOperations.Commands
+{
+	public class UpdateAuthorCommand
+	{
+        public int AuthorId { get; set; }
+        public UpdateAuthorModel Model { get; set; }
+
+        private readonly IBookStoreDbContext _context;
+
+        public UpdateAuthorCommand(IBookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Id == AuthorId);
+
+            if (author is null)
+            {
+                throw new InvalidOperationException("Yazar Bulunamadı");
+            }
+
+            author.Name = string.IsNullOrEmpty(Model.Name.Trim()) ? author.Name : Model.Name;
+            author.Surname = string.IsNullOrEmpty(Model.Surname.Trim()) ? author.Surname : Model.Surname;
+            author.Birthday = author.Birthday != default ? author.Birthday : Model.Birthday;
+
+            _context.SaveChanges();
+        }
+    }
+
+    public class UpdateAuthorModel
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
+
---
+++ Task7/Net Core Patika/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentValidation;
+using Net_Core_Patika.Application.GenreOperations.Commands.DeleteGenre;
+
+namespace Net_Core_Patika.Application.AuthorOperations.Commands
+{
+	public class UpdateAuthorCommandValidator : AbstractValidator<UpdateAuthorCommand>
+    {
+		public UpdateAuthorCommandValidator()
+		{
+			RuleFor(command => command.Model.Name).MinimumLength(2).When(x => x.Model.Name != string.Empty);
+            RuleFor(command => command.Model.Surname).MinimumLength(2).When(x => x.Model.Surname != string.Empty);
+        }
+	}
+}
+
